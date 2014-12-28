e6276a15a1e8252f4b318a3a65ae4ca849d3deb6
Jose Andres "Shea" Rosario <jose.shea.rosario@gmail.com>
2014-12-28 17:22:13 -0500

    Temporary Removal of Files
    Remove all files from the project

--- ToThePATHAndBack/gen/com/shea/tothepathandback/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.shea.tothepathandback;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- ToThePATHAndBack/gen/com/shearosario/tothepathandback/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.shearosario.tothepathandback;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
